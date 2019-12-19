7c59d935931174fe0587703fbad1a20fd38f9525
Quantum56 <bartleyzack@gmail.com>
2019-12-19 06:44:29 -0500

    edited everything

--- src/test.py
+++ src/test.py
@@ -1,22 +1,36 @@
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 import time
 from random import random
+from pylab import figure, axes, plot, title, show, legend, savefig
 
-testlist = [random()*10, random()*10]
 
-i = 1
-while i < 5:
-    testlist.append(random()*10)
-    i += 1
+def graph_create(mylist, type):
 
-for x in range(len(testlist)):
-    print(testlist[x], end=" ")
+    i = 1
+    while i < 10:
+        mylist.append(random())
+        i += 1
+
+    for x in range(len(mylist)):
+        print(mylist[x], end=" ")
+
+    figure(1, figsize=(12, 6))
+
+    plot(mylist)
+    title('Diversity of %s' %type, bbox={'facecolor': '0.8', 'pad': 5})
+    legend()
+    
+
+    #show()  # Actually, don't show, just save to foo.png
+    savefig('foo.png', bbox_inches='tight')
+
+# graph_create([random(), random()], 'f1')
 
 #df = pd.DataFrame({'x': range(1,101), 'y': np.random.randn(100)*15+range(1,101), 'z': (np.random.randn(100)*15+range(1,101))*2 })
  
-## Plot
+# Plot
 #plt.plot( 'x', 'y', data=df, marker='o', color='c')
 #plt.show()
 #time.sleep(5)
--- src/window.py
+++ src/window.py
@@ -2,13 +2,15 @@ import tkinter as tk
 from matplotlib import pyplot as plt
 import os
 from random import random
+from PIL import ImageTk, Image
+from test import graph_create
 
 class MainWindow(tk.Frame):
     counter = 0
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        self.button = tk.Button(self, text="Create new window", command=self.add_graph)
-        self.button.pack(side="top")
+        self.button1 = tk.Button(self, text="Graph Subplot", command=self.add_graph)
+        self.button1.pack(side="bottom")
         
         testlist = [random()*10,random()*10]
 
@@ -17,16 +19,21 @@ class MainWindow(tk.Frame):
             testlist.append(random()*10)
             i += 1
         
-        #plt.
 
     def add_graph(self):
+        graph_create([1, 2], 'f2')
         self.counter += 1
         t = tk.Toplevel(self)
-        t.wm_title("Window #%s" % self.counter)
-        l = tk.Label(t, text="This is window #%s" % self.counter)
-        l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        print(dir_path)
+        t.wm_title("Graph #%s" % self.counter)
+        l1 = tk.Label(t, text="Graph #%s" % self.counter)
+        l1.pack(side="top", fill="both", expand=True, padx=100, pady=100)
+        print(os.getcwd())
+        img = ImageTk.PhotoImage(Image.open("foo.png"))
+        l2 = tk.Label(t, image=img)
+        l2.pack(side="bottom", fill="both", expand=True)
+
+        # dir_path = os.path.dirname(os.path.realpath(__file__))
+        # print(dir_path)
         
 
 
